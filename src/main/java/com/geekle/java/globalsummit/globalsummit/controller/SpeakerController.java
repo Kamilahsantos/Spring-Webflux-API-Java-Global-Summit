bb938fcddae44968802482c9a0bf30af0bb9805c
Kamila fatima santos oliveira <kamila.fatima@amedigital.com>
2020-08-01 23:27:41 -0300

    remove response file

--- src/main/java/com/geekle/java/globalsummit/globalsummit/controller/SpeakerController.java
+++ src/main/java/com/geekle/java/globalsummit/globalsummit/controller/SpeakerController.java
@@ -3,8 +3,6 @@ package com.geekle.java.globalsummit.globalsummit.controller;
 
 import com.geekle.java.globalsummit.globalsummit.model.Speaker;
 import com.geekle.java.globalsummit.globalsummit.repository.SpeakerRepository;
-import com.geekle.java.globalsummit.globalsummit.response.ErrorResponse;
-import com.mongodb.DuplicateKeyException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
